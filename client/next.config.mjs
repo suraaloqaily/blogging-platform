08cef8055701656452458329ca8166fb3b848f2a
sura <62809305+suraaloqaily@users.noreply.github.com>
2024-10-29 22:15:58 +0300

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -8,6 +8,7 @@ const nextConfig = {
   },
    env: {
     NEXT_PUBLIC_SERVER_API_URL: process.env.NEXT_PUBLIC_SERVER_API_URL || 'http://localhost:5000/',
+    NODE_ENV: process.env.NODE_ENV || 'localhost',
   },
 };
 
