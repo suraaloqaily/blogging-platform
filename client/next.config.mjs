787f2171cfb947478266cc854c983a9d4f59ecd9
sura <62809305+suraaloqaily@users.noreply.github.com>
2024-10-29 21:54:58 +0300

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   images: {
     unoptimized: true, 
   },
+   env: {
+    NEXT_PUBLIC_SERVER_API_URL: process.env.NEXT_PUBLIC_SERVER_API_URL || 'http://localhost:5000/',
+  },
 };
 
 export default nextConfig;
