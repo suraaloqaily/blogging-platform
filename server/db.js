05fa9595f4768d1fd9723ba847e0e245f9acc431
suraaloqaily <aloqailysura@gmail.com>
2024-10-31 01:44:58 +0300

    add prisme

--- server/db.js
+++ server/db.js
@@ -6,6 +6,7 @@
 //   connectionString: process.env.POSTGRES_URL,
 // });
 // module.exports = pool;
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.POSTGRES_PRISMA_URL, {
