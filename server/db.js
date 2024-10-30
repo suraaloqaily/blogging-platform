6be19d802df06e432b6518b28e297a418bab52f0
suraaloqaily <aloqailysura@gmail.com>
2024-10-31 01:34:30 +0300

    add postgrs url

--- server/db.js
+++ server/db.js
@@ -8,7 +8,7 @@
 // module.exports = pool;
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   dialect: "postgres",
   dialectOptions: {
     ssl: {
--- server/server.js
+++ server/server.js
@@ -46,11 +46,6 @@ const syncDatabase = async () => {
   try {
     await sequelize.sync();
     console.log("Database synced successfully.");
-
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
   } catch (error) {
     console.error("Error syncing database:", error);
   }
