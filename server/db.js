54bdb4d65991745c60dcf381149617559e854726
suraaloqaily <aloqailysura@gmail.com>
2024-10-31 01:38:25 +0300

    add prisme

--- server/db.js
+++ server/db.js
@@ -8,7 +8,7 @@
 // module.exports = pool;
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+const sequelize = new Sequelize(process.env.POSTGRES_PRISMA_URL, {
   dialect: "postgres",
   dialectOptions: {
     ssl: {
