9add9bba3ed32aa606b08e558a4762bcff6d4d84
suraaloqaily <aloqailysura@gmail.com>
2024-10-31 00:35:26 +0300

    pg pool

--- server/db.js
+++ server/db.js
@@ -1,12 +1,8 @@
-const postgresPool = require("pg").Pool;
-require("dotenv").config();
+import pg from "pg";
 
-const pool = new postgresPool({
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  max: 10,
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
 });
 module.exports = pool;
