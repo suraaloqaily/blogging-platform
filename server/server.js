8f19338e5f34ba426a5413c933b9109ebcc8f573
suraaloqaily <aloqailysura@gmail.com>
2024-10-30 03:17:02 +0300

    publish app to github pages

--- server/server.js
+++ server/server.js
@@ -18,11 +18,10 @@ const corsOptions = {
 app.use(cors(corsOptions));
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",  
-  sameSite: "None", 
+  secure: process.env.NODE_ENV === "production",
+  sameSite: "None",
 };
 app.use(cookieParser(cookieOptions));
-
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -36,15 +35,15 @@ app.use("/auth", authRouter);
 app.use("/blogs", blogRouter);
 app.use("/user", userRouter);
 
-console.log("Environment:", process.env.NODE_ENV);
-console.log("Server Port:", process.env.PORT);
+// console.log("Environment:", process.env.NODE_ENV);
+// console.log("Server Port:", process.env.PORT);
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "test") {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-}
+// if (process.env.NODE_ENV !== "test") {
+//   app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+//   });
+// }
 
 module.exports = app;
