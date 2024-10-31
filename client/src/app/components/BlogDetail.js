c8cd4c5241d3f68164a8f4851f50d09ca6ea929d
sura <62809305+suraaloqaily@users.noreply.github.com>
2024-11-01 02:31:04 +0300

    Update BlogDetail.js

--- client/src/app/components/BlogDetail.js
+++ client/src/app/components/BlogDetail.js
@@ -61,7 +61,7 @@ const BlogDetail = ({ blogId }) => {
       const data = await apiService.likeBlog(blogId);
       console.log(data, "Dta insdie handle like comming from server");
       setIsLiked( data.liked );
-      setLikeCount(data.likeCounts)
+      setLikeCount(data.likeCount)
     } catch (error) {
       console.error("Error handling like:", error);
     }
