4240c0679024de1d5c2a2677dcdb7c4f341bdd5e
suraaloqaily <aloqailysura@gmail.com>
2024-10-31 21:48:54 +0300

    fixing adding comment

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,8 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   basePath: "/blogging-platform",
-   images: { unoptimized: true },
+  assetPrefix: "/blogging-platform/",
+  images: { unoptimized: true },
   env: {
     NEXT_PUBLIC_SERVER_API_URL: process.env.NEXT_PUBLIC_SERVER_API_URL,
   },
 };
+
+export default nextConfig;
