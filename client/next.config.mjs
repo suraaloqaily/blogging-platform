dcb65c666ad1ebbcf4818d2c136371773d50c3ba
sura <62809305+suraaloqaily@users.noreply.github.com>
2024-10-31 11:58:11 +0300

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -9,14 +9,6 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_SERVER_API_URL: process.env.NEXT_PUBLIC_SERVER_API_URL,
   },
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-      "/login": { page: "/login" },
-      "/register": { page: "/register" },
-      // Add other paths as necessary
-    };
-  },
 };
 
 export default nextConfig;
