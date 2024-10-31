44b3afc568fee75103a26cbbb207e97c91c883b8
sura <62809305+suraaloqaily@users.noreply.github.com>
2024-10-31 12:09:00 +0300

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
   output: "export",
   basePath: "/blogging-platform",
   assetPrefix: "/blogging-platform/",
-  exportTrailingSlash: true,
   images: { unoptimized: true },
   env: {
     NEXT_PUBLIC_SERVER_API_URL: process.env.NEXT_PUBLIC_SERVER_API_URL,
